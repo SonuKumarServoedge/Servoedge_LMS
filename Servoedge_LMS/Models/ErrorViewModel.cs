e582bd5fa6fb9a63830016dd6814ed56dafae709
SonuKumarServoedge <sonu.kumar@servoedge.com>
2025-01-22 18:12:23 +0530

    Add project files.

---
+++ Servoedge_LMS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Servoedge_LMS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
